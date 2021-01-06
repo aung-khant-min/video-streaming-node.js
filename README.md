d7eb3ea3c94ded2691176816c3c6213fdd476941
aung-khant-min <aungkhantmin@ucsm.edu.mm>
2021-01-06 21:40:02 +0630

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Video Streaming with NodeJs
+
+## How to run the project
+
+[Node.js](https://nodejs.org/) is required to run this project.
+
+First, install the packages
+
+```bash
+npm install
+```
+
+Start the Server
+
+```bash
+ npm start
+```
+
+Or
+
+```bash
+ npm run dev
+```
+
+See Result on [http://localhost:8000/](http://localhost:8000/)
---
+++ app.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const express = require('express')
+const app = express()
+
+const port = 8000 || process.env.PORT
+
+app.use(express.static(__dirname + '/public/'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/video', (req, res) => {
+
+    //Get the video file name query
+    const file = req.query.file
+
+    // Ensure there is a range given for the video
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send('Requires Range header');
+    }
+
+    // get video stats 
+    const videoPath = 'videos/' + file;
+    const videoSize = fs.statSync('videos/' + file).size;
+
+    // Parse Range
+    // Example: 'bytes=32324-'
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+    // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': contentLength,
+        'Content-Type': 'video/mp4',
+    };
+
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
+
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+});
+
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
