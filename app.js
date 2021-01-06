5deb0e7aff38e4b2a0acd0970df624caaa6fbb0a
aung-khant-min <aungkhantmin@ucsm.edu.mm>
2021-01-06 22:19:24 +0630

    mistake

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 
-const port = 8000 || process.env.PORT
+const port = process.env.PORT || 8000 
 
 app.use(express.static(__dirname + '/public/'))
 
